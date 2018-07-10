d5f6206fb30b25c1698ee564167b9164a3e0c1f9
sherlyfekay <sherlyfekay@gmail.com>
2018-07-10 17:02:36 +0700

    update history

--- api/controllers/ordersoffers.js
+++ api/controllers/ordersoffers.js
@@ -236,13 +236,23 @@ exports.oo_get_oo_by_idagent = async (req, res, next) => {
         {
             $unwind: '$patient'
         },
-        
+        {
+            $lookup: {
+                from: 'addresses',
+                localField: 'id_address',
+                foreignField: '_id',
+                as: 'address'
+            }
+        },
+        {
+            $unwind: '$address'
+        },
         {
             $project: {
                 _id: 1,
                 jenis: {$cond: [{$eq:['$jenis', 1]}, 'Pemesanan', 'Penawaran']},
                 nama_pasien: '$patient.nama_lengkap',
-                // alamat_pasien: '$address.alamat_lengkap',
+                alamat_pasien: '$address.alamat_lengkap',
                 created_at: 1
             }
         }
